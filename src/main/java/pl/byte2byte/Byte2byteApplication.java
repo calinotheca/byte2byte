270178bda9e0f14fe7912137357df5a8298b0c82
Calinotheca <jakub0002@gmail.com>
2020-12-04 20:46:28 +0100

    first commit

---
+++ src/main/java/pl/byte2byte/Byte2byteApplication.java
@@ -0,0 +1,13 @@
+package pl.byte2byte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Byte2byteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Byte2byteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/byte2byte/Byte2byteApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.byte2byte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Byte2byteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
